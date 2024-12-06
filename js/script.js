caff9e9fc33a3050a97995ac2b672553941366d3
Fenzit <tuglukov.7.diyas@gmail.com>
2024-12-07 00:49:31 +0500

    7comlplete

--- js/script.js
+++ js/script.js
@@ -50,4 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
-  
+function redirectToWebsite() {
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+}
